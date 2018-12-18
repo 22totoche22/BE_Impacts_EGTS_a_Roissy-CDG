c6f0584d9faa3e64a762b78c26a1f60950776bd6
auriane <auriane.dubois@outlook.fr>
2018-12-18 16:47:50 +0100

    legere modif inutile mais jolie dans distance 3D

--- pente.ml
+++ pente.ml
@@ -5,7 +5,7 @@ module Del = Delaunay
 let distance3D = fun point1 point2 ->
   let diffX = float (point1.Map.x - point2.Map.x) in
   let diffY = float (point1.Map.y - point2.Map.y) in
-  let diffZ = point1.Map.z -.point2.Map.z in
+  let diffZ = point1.Map.z -. point2.Map.z in
   let distance = sqrt (diffX *. diffX +. diffY *. diffY +. diffZ *. diffZ) in
   distance;;
 
