3078a2e91b475cf279c32895b5194804ada572a3
auriane <auriane.dubois@outlook.fr>
2018-12-29 16:26:37 +0100

    ajout d'une fonction pour calculer l'altitude min et max sur l'aeroport

--- pente.ml
+++ pente.ml
@@ -1,6 +1,8 @@
 module Map = Lfpg_map
 module Del = Delaunay
-
+module Pivot = Pivot
+module Geo = GeometrieTriangle
+  
 (* retourne la distance entre 2 points en géometrie 3d *)
 let distance3D = fun point1 point2 ->
   let diffX = float (point1.Map.x - point2.Map.x) in
@@ -27,3 +29,56 @@ let altitudePoint = fun point triangle ->
   let a,b,c = triangle.Del.equa in
   let z = a *. (float point.Map.x) +. b *. (float point.Map.y) +. c in
   z;;
+
+(* calcul min d'une liste de float *)
+let mini = fun liste ->
+  let minimum = ref (infinity) in
+  let rec loop = fun listep ->
+    match listep with
+	[] -> ()
+      | z::reste ->
+	if z < !minimum then minimum := z;
+	loop reste;
+  in loop liste;
+  !minimum;;
+
+(* calcul max d'une liste de float *)
+let maxi = fun liste ->
+  let maximum = ref (neg_infinity) in
+  let rec loop = fun listep ->
+    match listep with
+	[] -> ()
+      | z::reste ->
+	if z > !maximum then maximum := z;
+	loop reste;
+  in loop liste;
+  !maximum;;
+
+
+(* calcule le point le plus haut et le plus bas de l'aeroport *)
+let calculAltiMinMax = fun listePointAeroport delaunay ->
+  let listeAlti = ref [] in
+  let rec loop = fun listePoint ->
+    match listePoint with
+	[] -> ()
+      | point::reste ->
+	let listeTriangle = Geo.dansQuelTriangle point delaunay in
+	begin
+	  match listeTriangle with
+	      [] -> ()
+	    | triangle::queue ->
+	      let z = altitudePoint point triangle in
+	      listeAlti := z::(!listeAlti);
+	end;
+	loop reste;
+  in loop listePointAeroport;
+  let zmin = mini !listeAlti in
+  let zmax = maxi !listeAlti in
+  zmin,zmax;;
+
+(* calcul de l'altitude Min et Max sur l'aeroport *)
+(* ATTENTION liste de point de départ à vérifier *)
+let listePoint = Map.point_xyz_points Map.points_alti
+let delau = Pivot.triangle_equa 
+let zmin,zmax = calculAltiMinMax listePoint delau
+let () = Printf.printf "min : %f max : %f denivele : %f\n" zmin zmax (zmax -. zmin)
