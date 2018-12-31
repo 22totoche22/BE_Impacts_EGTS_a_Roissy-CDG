16727514b32413ece2c4fbeb8f641bc2315853d7
22totoche22 <adrien.caillet2@gmail.com>
2018-12-31 15:12:40 +0100

    begins the backtrack code

--- backtrack2.ml
+++ backtrack2.ml
@@ -20,39 +20,39 @@ let rencontre pointA pointB =
   (sqrt ( dis_ab)) < (2. *. rayon) ;; (* |R-R'| < AB < R+R' *)
 
 (* renvoie true si l'avion est en conflit au temps t avec les autres avions déjà resolus *)
-let avion_conflit fenetre trajectoire_avion t =
+let avion_conflit fenetre trajectoire_avion t_avion t_fenetre =
   let conflit = ref false in
   let taille = Array.length trajectoire_avion in
   let rec rec_conflit i =
     if !conflit || i = taille then ()
     else
       begin
-	conflit := rencontre fenetre.(i).(t) trajectoire_avion.(t);
+	conflit := rencontre fenetre.(i).(t_fenetre) trajectoire_avion.(t_avion);
 	rec_conflit (i + 1) ;
       end in
   rec_conflit 0;
   !conflit;;
-(*A MODIFIER  pour les avions, leur time propre est time + heduredebut *) 
+
 (*
 (* rajoute la trajectoire d'un avion sans conflit *)
-let ajout_avion_resolu avion =
+let ajout_avion_resolu avion fenetre ligne dt triangulation =
   let tab_trajectoire = trajectoire avion in
+  let length = Array.length tab_trajectoire in
   let time_debut = avion.Map.h_dep in
-  let rec solve_conflit position avion temps_t vitesse =
-    let position = position_temps tab_trajectoires temps_t 
-      avion_conflit fenetre trajectoire_avion
-
-
-      Sans_conflit position temps
-      next_position = 
-
-    append
-  
-    next_position = fin 
-  || solve_conflit next_position next_temps_t next_vitesse
-  || solve_conflit position next_temps_t 0
-
-
+  let debut = tab_trajectoire.(0) in
+  let fin = tab.trajectoire.(length -1) in
+  let vitesse = ref 0. in
+  let rec solve_conflit position temps_t vitesse =
+    let position = position_temps tab_trajectoire temps_t in
+    if not (avion_conflit fenetre trajectoire_avion temps_t (time_debut + temps_t))
+    then Array.set fenetre.(ligne) temps_t position 
+    else  vitesse:= 0.;
+    let new_position = List.hd (
+      Accel.calculTrajectoireEntre2points  position next_position avion.Map.flight_category avion.Map.masse triangulation compteurTempsA5s timeSimulation vitesseAvant avion.Map.flight_stand) in
+    new_position = fin  || solve_conflit new_position (temps_t + dt) !vitesse ||  solve_conflit position (temps_t +dt) !vitesse in
+  solve_conflit debut 0 !vitesse;;
+    
 *)
+
 
 
